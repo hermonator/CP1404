2d8f9788a3e8f23c46459df0e3c1c55bbbdf3b3b
Jesse Hermon <hermonator3@gmail.com>
2016-04-30 21:05:53 +1000

    nearly finished workshop

---
+++ Workshop 8/Guitar.py
@@ -0,0 +1,25 @@
+class Guitar:
+
+    def __init__(self,name=" ",year=0,cost=0):
+
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
+    def __str__(self):
+        return '{} ({}) : {}'.format(self.name,self.year,self.cost)
+
+    def get_age(self):
+
+        age = 2016 - self.year
+
+        return age
+
+    def is_vintage(self):
+
+        age = self.get_age()
+        is_old = False
+        if age >= 50:
+            is_old = True
+        return is_old
---
+++ Workshop 8/guitars.py
@@ -0,0 +1,13 @@
+__author__ = "Jesse Hermon"
+
+from guitar import Guitar
+
+def main():
+    print('My guitars!')
+    guitar1 = Guitar("Fender Stratocaster",1967,765.4)
+
+
+
+
+
+main()
