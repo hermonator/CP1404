413dd10281255b0608964a0bf95e00301ff426f0
jc259420 <hermonator3@gmail.com>
2016-05-03 11:38:54 +1000

    Finished workshop finally

--- Workshop 8/guitar_test.py
+++ Workshop 8/guitar_test.py
@@ -1,10 +1,24 @@
 from guitar import Guitar
 
 def main():
-    print('My guitars!')
-    guitar1 = Guitar("Fender Stratocaster",1967,765.4)
-    print (guitar1.get_age())
-    print (guitar1.is_vintage())
+
+    guitar1 = Guitar("Fender Stratocaster",1923,765.4)
+    print ('guitar1.get_age() - Expected 93. Got {}'.format(guitar1.get_age()))
+    print ('guitar1.is_vintage - Expected True. Got {}'.format(guitar1.is_vintage()))
+
+    print('')
+
+    guitar2 = Guitar("Fender Stratocaster",1967,765.4)
+    print ('guitar2.get_age() - Expected 49. Got {}'.format(guitar2.get_age()))
+    print ('guitar2.is_vintage - Expected False. Got {}'.format(guitar2.is_vintage()))
+
+    # testing out the float value cost.
+    
+    # Works!!
+        #print (guitar2.cost)
+
+
+    #   print('{:.2f}'.format(guitar2.cost))
 
 
 
--- Workshop 8/guitars.py
+++ Workshop 8/guitars.py
@@ -3,8 +3,32 @@ __author__ = "Jesse Hermon"
 from guitar import Guitar
 
 def main():
+
     print('My guitars!')
-    guitar1 = Guitar("Fender Stratocaster",1967,765.4)
+
+    # A control structure to input and store the users guitars.
+    guitars_list = []
+    guitar_name ='a'
+    #guitar_counter = 0
+    #guitar_title = 'Guitar '
+    while guitar_name != '':
+        guitar_name = input('Name: ')
+        if guitar_name != '':
+            #guitar_counter = guitar_counter + 1
+            guitar_year = int(input('Year: '))
+            guitar_cost = float(input('Cost: '))
+            guitars_list.append(Guitar(guitar_name,guitar_year,guitar_cost))
+            print('{} added'.format(guitars_list[-1]))
+
+            
+    # A control structure to display the guitars.
+    for i, guitar in enumerate(guitars_list):
+
+        vintage_string = ''
+        if guitar.is_vintage():
+            vintage_string = '(vintage)'
+
+        print('Guitar {}: {:>20} ({}), worth ${:10,.2f} {}'.format(i + 1, guitar.name,guitar.year,guitar.cost,vintage_string))
 
 
 
